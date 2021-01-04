9e7f5a6336917d6bdc01148a595a2fe84bd68374
ismailonur <ismailonur15@gmail.com>
2021-01-04 18:19:46 +0300

    input alınarak işlemler yapıldı ve ekrana çıktı gösterildi

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+var sifreCozBtn=document.getElementById("sifreCozButton");
+var sifreOlusturBtn=document.getElementById("sifreOlusturButton");
+
+sifreCozBtn.onclick=function(){
+    var ad=document.getElementById("SifreCoz");
+    //window.alert("Hoşgeldin: "+ad.value);
+    document.getElementById("demo").innerHTML = ad.value;
+    
+    sifre = ad.value;
+
+    RunSifreCoz()
+}
+
+sifreOlusturBtn.onclick=function(){
+    var ad=document.getElementById("SifreOlustur");
+    //window.alert("Hoşgeldin: "+ad.value);
+    document.getElementById("demo").innerHTML = ad.value;
+    
+    metin = ad.value;
+
+    RunSifreOlustur()
+}
+
+var sifre = ""
+var metin = ""
+
+
 var array = [
     ["A", "B", "C", "D", "E"],
     ["F", "G", "H", "I", "J"],
@@ -7,30 +34,33 @@ var array = [
 ];
 
 var harfler = {
-    A: "11", B: "12", C: "13", D: "14", E: "15", F: "21", G: "22", H: "23", I: "24", J: "25", K: "31", L: "32", M: "33", N: "34", O: "35", P: "41", R: "42", S: "43", T: "44", U: "45", X: "51", W: "52", V: "53", Y: "54", Z: "55"
+    A: "11", B: "12", C: "13", D: "14", E: "15",
+    F: "21", G: "22", H: "23", I: "24", J: "25",
+    K: "31", L: "32", M: "33", N: "34", O: "35",
+    P: "41", R: "42", S: "43", T: "44", U: "45",
+    X: "51", W: "52", V: "53", Y: "54", Z: "55"
 };
 
+function RunSifreCoz(){
+    var regex = /\d\d/g;
+    var resSifre = sifre.match(regex);
+    var resSifreSonuc = "";
 
-var sifre = "44423324322444114254"
-var metin = "TRMILITARY"
-
-var regex = /\d\d/g;
-
-var resSifre = sifre.match(regex);
-var resMetin = metin.split("");
-
-
-var resSifreSonuc = "";
-var resMetinSonuc = "";
-
-(function () {
     resSifre.forEach((x) => {
         resSifreSonuc += (array[x[0] - 1][x[1] - 1])
     })
-    console.log(resSifreSonuc);
+    document.getElementById("demo").innerHTML = resSifreSonuc
+    resSifreSonuc="";
+}
+
+function RunSifreOlustur(){
+    var resMetin = metin.split("");
+    var resMetinSonuc = "";
 
     resMetin.forEach((x) => {
         resMetinSonuc += harfler[x]
     })
-    console.log(resMetinSonuc);
-}())
+    document.getElementById("demo").innerHTML = resMetinSonuc;
+    resMetinSonuc="";
+}
+
